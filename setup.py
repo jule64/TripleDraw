8375d04020ac1bd7ef4a6f92e33f24525f4498d4
jule64 <jule64@gmail.com>
2016-02-26 04:05:37 +0000

    added setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+        name='tripledraw',
+        version='0.1',
+        py_module=['tripledraw'],
+        install_requires=[
+            'click>=4.0',
+        ],
+        author='julien monnier',
+        author_email='jule64@gmail.com',
+        entry_points='''
+        [console_scripts]
+        tripledraw=tripledraw:main
+        ''',
+)
