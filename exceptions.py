748e5c6c213b8fc971dc353cc3b3efb4100f8a07
jule64 <jule64@gmail.com>
2016-02-29 21:26:15 +0000

    added abort logic

---
+++ exceptions.py
@@ -0,0 +1,5 @@
+
+
+
+class UnsurportedInputValueException(RuntimeError):
+    pass
--- tripledraw.py
+++ tripledraw.py
@@ -6,6 +6,24 @@ import math
 import logging
 
 
+
+help_msgs={'starting-cards':    'Any number of predefined cards to have in your starting hand. '
+                                'The default is no predefined cards (i.e. all 5 starting cards are randomly '
+                                'generated on each simulation).  See example 1 and 2 printed above for details.',
+           'number-of-draws':   'The number of draws to simulate. By default 3 which corresponds to a full hand.  '
+                                'If you want to simulate a late stage, for example when there is only one draw left, '
+                                'simply pass the number 1.  Use 0 to simulate preflop stage, useful if for example '
+                                'you want to know the odds of being dealt a 10 low pre flop.',
+           'target':            'The hand you want to simulate the odds of. For example to target 8 Low simply pass 8 to this '
+                                'option. The default target is J (Jack low).',
+           'simulations':       'The number of simulations to run. By default it is 50,0000 which offers '
+                                'roughly 0.1% precision around the true odds.',
+           'number-procs':      'The number of parallel processes used to run the simulations. By defaults this number is '
+                                'equal to the number of cores available on your machine. Set this value to 0 to run the '
+                                'application single threaded.'
+           }
+
+
 # logger setup
 FORMAT = '%(asctime)-15s %(message)s'
 logging.basicConfig(format=FORMAT)
@@ -13,26 +31,16 @@ logger = logging.getLogger('tripledraw')
 logger.setLevel(logging.INFO)
 
 
+
 @click.command()
-@click.option('--starting-cards','-s', prompt=True, default='',
-              help='Any number of predefined cards to have in your starting hand.'
-                    ' The default is no predefined cards (i.e. all 5 starting cards are randomly'
-                    ' generated on each simulation).  See example 1 and 2 printed above for details.')
-@click.option('--number-of-draws','-d', prompt=True, default=0,
-              help='The number of draws to simulate. By default 3 which corresponds to a full hand.  '
-                   'If you want to simulate a late stage, for example when there is only one draw left,'
-                   ' simply pass the number 1.  Use 0 to simulate preflop stage, useful if for example'
-                   ' you want to know the odds of being dealt a 10 low pre flop.')
+@click.option('--starting-cards','-s', prompt=True, default='', help=help_msgs['starting-cards'])
+@click.option('--number-of-draws','-d', prompt=True, default=0, help=help_msgs['number-of-draws'])
 @click.option('--target', '-t', prompt=True, default='J',
-              help='The hand you want to simulate the odds of. For example to target 8 Low simply pass 8 to this'
-                   'option. The default target is J (Jack low).')
+              help=help_msgs['target'])
 @click.option('--simulations','-n', default=50000,
-              help='The number of simulations to run. By default it is 50,0000 which offers '
-                   'roughly 0.1% precision around the true odds.')
+              help=help_msgs['simulations'])
 @click.option('--number-procs','-p', default=cpu_count(),
-              help='The number of parallel processes used to run the simulations. By defaults this number is '
-                   'equal to the number of cores available on your machine. Set this value to 0 to run the '
-                   'application single threaded.')
+              help=help_msgs['number-procs'])
 def main(starting_cards, number_of_draws, target, simulations, number_procs):
 
     """Welcome to tripledraw!
@@ -49,6 +57,14 @@ def main(starting_cards, number_of_draws, target, simulations, number_procs):
 
     **************************************************************************
     """
+    if(starting_cards != ''):
+        print '[!] Simulation Aborted:  Adding starting cards is not currently supported.  Apologies'
+        return
+    if(target!='J'):
+        print "[!] Simulation Aborted:  Only Jack low target (J) is currently supported.  Apologies"
+        return
+
+
     if(number_procs<=1):
         run_single_threaded(starting_cards, number_of_draws, target, simulations)
     else:
